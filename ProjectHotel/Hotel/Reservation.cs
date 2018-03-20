81d9ec887981ece807ef0648185824a50521fae3
Kastrioti12 <36193137+Kastrioti12@users.noreply.github.com>
2018-03-20 15:23:18 +0100

    Reservation

---
+++ ProjectHotel/Hotel/Reservation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+
+    enum RoomView { Sea, Road, Garden };
+    class Reservation 
+    {
+        //String NameOfGuest;
+        int NumberOfRoom;
+        public RoomView roomView;
+        
+
+
+        public Reservation( int roomNumber, RoomView _roomView)
+        {
+            
+            NumberOfRoom = roomNumber;
+            _roomView = roomView;
+        }
+
+        public void printReservation()
+        {
+            Console.WriteLine("Room number {0} is reserved under the name {1} with the view of {2}", NumberOfRoom, roomView);
+
+        }
+
+        public int getRoomNumber()
+        {
+            return NumberOfRoom;
+        }
+    }
+}
