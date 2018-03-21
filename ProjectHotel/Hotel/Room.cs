c96a9dc83dcff8e857a54494100d43b69e1e06e7
Kastrioti12 <36193137+Kastrioti12@users.noreply.github.com>
2018-03-21 01:44:41 +0100

    Room

---
+++ ProjectHotel/Hotel/Room.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Room
+    {
+        public int idRoom;
+        public bool empty;
+
+        public string IDRoom
+        {
+            get;
+            set;
+        }
+
+        public Room()
+        {
+            this.idRoom = 0;
+
+            this.empty = false;
+        }
+        public Room(int idRoom, bool empty)
+        {
+            this.idRoom = idRoom;
+            this.empty = empty;
+
+        }
+
+        public bool isEmpty()
+        {
+            return empty;
+        }
+        public void setEmpty(bool empty)
+        {
+            this.empty = empty;
+        }
+
+        //Virtual methods are used when we expect from derived classes to change.
+        public virtual void printRoom()
+        {
+            Console.WriteLine(idRoom + " " + empty);
+        }
+    }
+}
