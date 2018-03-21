849f3c72f5929e209d3cc0cdf19e80937f2de6bc
Kastrioti12 <36193137+Kastrioti12@users.noreply.github.com>
2018-03-21 01:45:19 +0100

    TRoom

---
+++ ProjectHotel/Hotel/Twin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    abstract class Twin:Room
+    {
+        public override void printRoom()
+        {
+            Console.WriteLine(idRoom + " " + empty + " -Twin Room");
+        }
+    }
+}
