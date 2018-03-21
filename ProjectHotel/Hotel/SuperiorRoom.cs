85e59736fe9425f32ee74cd53c54856cac47456e
Kastrioti12 <36193137+Kastrioti12@users.noreply.github.com>
2018-03-21 01:45:06 +0100

    SRoom

---
+++ ProjectHotel/Hotel/SuperiorRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    abstract class SuperiorRoom:Room //Polymorphisem duke perdorur tek klassa Room metoden virtual dhe duke e bere
+        //override metoden printRoom ngjashem si tek \Room
+    {
+        public override void printRoom()
+        {
+            Console.WriteLine(idRoom + " " + empty + " -Superior Room");
+        }
+    }
+}
