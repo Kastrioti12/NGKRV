f261c8c79ac09bf1df4d9c516c46f2f25b73ec42
Kastrioti12 <36193137+Kastrioti12@users.noreply.github.com>
2018-03-22 17:38:07 +0100

    SingeltonDemo

---
+++ ProjectHotel/Hotel/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        //singeltone controls currens access to the resource.
+        // ensures that only one instance of a class exist.
+        public static Singleton getInsance // static property to return a single instance of object.
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("|Counter value"+counter.ToString());
+        }
+        public void printDetailes(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
